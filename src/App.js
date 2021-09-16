400d5d559484fa772f8bfa19503e0d6c19a98e19
golibjon94 <golib94jon@gmail.com>
2021-09-16 11:22:50 +0500

    finance svg map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Map from "./components/Map"
+import RegionsData from "./components/RegionsData"
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>  
+<Switch>
+  <Route  exact path="/" >
+  <Map/>
+  </Route>
+  <Route  exact path="/regionsData/:id" >
+<RegionsData/>
+  </Route>
+  
+</Switch>
+</Router>
+    
     </div>
   );
 }
---
+++ src/components/RegionsData.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import "./regionsData.css";
+import { allDatas } from "../allDatas";
+import { useParams } from "react-router-dom";
+
+function RegionsData() {
+  const [idd, setIdd] = useState(null);
+  const [data, setdata] = useState({})
+  const [showDatas, setShowDatas] = useState(false)
+  const [categories, setcategories] = useState([]);
+  const { id } = useParams();
+  const filteredDatas = allDatas.filter((item) => item.id === id);
+
+  useEffect(() => {
+    filteredDatas.map((item) => setcategories(item.infos));
+  }, []);
+const getDatas=(id)=>{
+   setShowDatas(!showDatas)
+setdata({
+   shaharNomi:"Bo'stonliq",
+   id,
+   aholisi:1000,
+   kambagallar:"juda kop",
+   boylar:"juda kam"
+})
+}
+  return (
+    <div>
+      {showDatas &&
+      <div>
+         Shahar nomi:{data.shaharNomi},
+         <br/>
+         Id:{data.id},
+         <br/>
+         Shahar aholisi:{data.aholisi},
+         <br/>
+         Kambagallar soni:{data.kambagallar},
+         <br/>
+         Boylar soni:{data.boylar},
+         <br/>
+      </div>
+}
+
+      <svg
+        id="tashkent"
+        baseprofile="tiny"
+        height="1150"
+        stroke="#ffffff"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        version="1.2"
+        viewbox="0 0 1000 652"
+        width="1800"
+      >
+        <svg>
+          {categories?.map((item) => (
+            <path
+            onClick={(id)=>{getDatas(item.id)}}
+             id={item.id} d={item.d} />
+          ))}
+        </svg>
+        <circle cx="673.4" cy="626" id="0"></circle>
+        <circle cx="637.6" cy="506" id="1"></circle>
+        <circle cx="636" cy="498.9" id="2"></circle>
+      </svg>
+    </div>
+  );
+}
+
+export default RegionsData;
